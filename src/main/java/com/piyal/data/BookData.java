e0a758715ed07e5aae1623307a079c920b5f9a4b
JunjiaWangUSF <112034657+JunjiaWangUSF@users.noreply.github.com>
2025-02-11 13:47:39 -0800

    Update BookData.java

--- src/main/java/com/piyal/data/BookData.java
+++ src/main/java/com/piyal/data/BookData.java
@@ -3,6 +3,5 @@ package com.piyal.data;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
-public record BookData(String name, String author, Double price, Integer totalPage)
-{
+public record BookData(String name, String author, Double price, Integer totalPage){
 }
